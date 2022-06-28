91b10d5da6c1f4ef2cb6054965258c6c6cc89d92
dlawton20-devops <77588468+dlawton20-devops@users.noreply.github.com>
2022-06-28 12:23:06 +0100

    Update provider.tf

--- terraform/statebucket/provider.tf
+++ terraform/statebucket/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.12"
 }
 provider "google" {
-  credentials = file(var.gcp_auth_file)
+  credentials = var.gcp_auth_file
   project     = var.gcp_project
   region      = var.gcp_region
 }
